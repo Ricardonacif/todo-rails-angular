ec4e93a2e7015c33e293c8f8a7104dffc8821186
Ricardo Nacif <nacif.ricardo@gmail.com>
2014-08-01 15:17:54 -0300

    angular production mangle fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,9 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  config.assets.js_compressor = Uglifier.new(:mangle => false)
+
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
